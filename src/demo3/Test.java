efd3c615cd21fd87189a145fb4512ee95e681c6e
demo3 <demo3@liqin.com>
2020-11-18 15:22:02 +0800

    输入输出测试

--- src/demo3/Test.java
+++ src/demo3/Test.java
@@ -1,11 +1,12 @@
 package demo3;
 
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("测试");
-		System.out.println("测试上传");
-		System.out.println("测试2");
-		System.out.println("测试3测试代充商店出售");
-		System.out.println("测测试三十四hi是否hi时代峻峰is地方");
+		Scanner input = new Scanner(System.in);
+		System.out.print("请输入姓名:");
+		String name = input.next();
+		System.out.println("您输入的姓名是:"+name);
 	}
 }
